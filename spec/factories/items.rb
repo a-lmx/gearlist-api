7188f5cbd4279dec72dd77696c3ea6c5e7b15715
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 13:16:30 -0700

    installed rspec

---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :item do
+    name "ULA Circuit"
+    category "backpack"
+    weight 800
+  end
+
+end
---
+++ spec/factories/lists.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :list do
+    name "JMT 2013"
+    user_id 1
+  end
+
+end
