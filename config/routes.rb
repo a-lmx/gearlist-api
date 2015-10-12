c3cc6a1774395e61f120152c7c7f5afd3022bde4
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 14:43:20 -0700

    added items#show route; removed commented scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index]
+      resources :items, only: [:index, :show]
     end
   end
-  # scope "/api" do
-  # end
 end
