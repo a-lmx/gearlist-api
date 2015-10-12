dba8484b69d7f68610567999dbd9f37a10856503
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 13:23:46 -0700

    ran List model creation migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012200235) do
+ActiveRecord::Schema.define(version: 20151012201255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20151012200235) do
     t.string   "name"
     t.string   "category"
     t.integer  "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
