12c31a00527ed3536ca682402832e46a763a0d9a
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 14:55:18 -0700

    added tests for json response object from #index

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,9 +3,38 @@ require 'rails_helper'
 RSpec.describe Api::V1::ItemsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+
+    before :each do
+      item1 = create :item, name: "circuit"
+      item2 = create :item, name: "burn"
+
       get :index
+      @items = JSON.parse response.body
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    context "the returned JSON object" do
+      it "is an array" do
+        expect(@items).to be_an_instance_of Array
+      end
+
+      it "is equal in length to the number of Items in the db" do
+        expect(@items.length).to eq(Item.count)
+      end
+
+      it "includes name, category, and weight" do
+        expect(@items.first.keys).to eq [
+          "id",
+          "name", 
+          "category", 
+          "weight",
+          "created_at",
+          "updated_at"
+          ]
+      end
     end
   end
 
