f85e2cf6fca9ad3af0085d0d64a408b6f4b1e554
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 13:24:08 -0700

    wrote tests and validations for item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ActiveRecord::Base
+  # Validations ----------------------------------------------------------------
+  validates :name, :category, :weight, presence: true
+  validates :weight, numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  let(:item) { build :item }
+
+  describe "validations" do
+    [
+      :name,
+      :category, 
+      :weight
+    ].each do |property|
+      it "requires a #{property}" do
+        item[property] = nil
+        item.valid?
+        expect(item.errors.keys).to include(property)
+      end
+    end
+
+    it "doesn't allow a weight of 0" do
+      item.weight = 0
+      item.valid?
+      expect(item.errors.keys).to include(:weight)
+    end
+
+    it "doesn't allow a negative weight" do
+      item.weight = -10
+      item.valid?
+      expect(item.errors.keys).to include(:weight)
+    end
+
+    it "doesn't allow a decimal weight" do
+      item.weight = 10.5
+      item.valid?
+      expect(item.errors.keys).to include(:weight)
+    end
+  end
 end
