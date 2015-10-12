6cb5038a15b27066f581fddb9f024f1e56157498
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 13:28:32 -0700

    created ListItem model

---
+++ db/migrate/20151012202736_create_list_items.rb
@@ -0,0 +1,11 @@
+class CreateListItems < ActiveRecord::Migration
+  def change
+    create_table :list_items do |t|
+      t.integer :item_id
+      t.integer :list_id
+      t.integer :quantity
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012201255) do
+ActiveRecord::Schema.define(version: 20151012202736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20151012201255) do
     t.string   "name"
     t.string   "category"
     t.integer  "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "list_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "list_id"
+    t.integer  "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
