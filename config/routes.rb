9c38a953ffe127c37b9a7466a0b8ded8acc374ff
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 15:28:34 -0700

    added routes for MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show]
+      resources :users, only: [] do
+        resources :lists, only: [:index]
+      end
+
+      resources :lists, only: [:create] do
+        resources :items, only: [:index]
+      end
+
+      # :index and :show are for debugging
+      resources :items, only: [:create, :index, :show]
+
+      resources :list_items, only: [:create, :destroy]
     end
   end
 end
