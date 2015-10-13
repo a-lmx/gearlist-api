118d10ee0d8cb0b4ffc2fdcb3990b2b2082a1a98
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-13 10:14:02 -0700

    updated routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,10 @@ Rails.application.routes.draw do
         resources :lists, only: [:index]
       end
 
-      resources :lists, only: [:show, :create] do
-        # resources :items, only: [:index]
+      resources :lists, only: [:index, :show, :create] do
+        resources :items, only: [:index]
       end
-      # get '/lists/:list_id/items', to: 'items#by_list', as: 'items_by_list'
 
-      # :index and :show are for debugging
       resources :items, only: [:create, :index, :show]
 
       resources :list_items, only: [:create, :destroy]
