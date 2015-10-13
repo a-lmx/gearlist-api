00e27f6523c2f03d9d25c1490d688ffe01510388
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 22:38:20 -0700

    added has_many: through: association

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ActiveRecord::Base
   # Associations ---------------------------------------------------------------
   has_many    :list_items
+  has_many    :items, through: :list_items
   belongs_to  :user
   # Validations ----------------------------------------------------------------
   validates :name, :user_id, presence: true
