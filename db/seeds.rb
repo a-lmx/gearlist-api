280b699004a846fd92224d9418b3b2f5b80db10b
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-12 19:28:27 -0700

    added seed data for User, Item, List

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+users = [
+  { uid: "1234", 
+    username: "Grizzly" },
+  { uid: "2345", 
+    username: "Slow Poke" },
+  { uid: "3456", 
+    username: "Fluff Ball" }
+]
+
+users.each do |user|
+  User.create(user)
+end
+
+items = [
+  { name: "MLD Burn pack - custom w/ two hip pockets", 
+    category: "backpack", 
+    weight: 525 },
+  { name: "Hefty trash compactor bag", 
+    category: "pack liner", 
+    weight: 62 },
+  { name: "Sea to Summit ultra-sil nano dry sack - 8L", 
+    category: "clothing bag", 
+    weight: 27 },
+  { name: "Western Mountaineering Versalite 10° - 5'6\"", 
+    category: "sleeping bag", 
+    weight: 904 },
+  { name: "Katabatic Palisade quilt 30°", 
+    category: "sleeping bag", 
+    weight: 486 },
+  { name: "Therm-a-rest Z-lite small, minus 2 sections", 
+    category: "sleeping pad", 
+    weight: 241 },
+  { name: "Patagonia Houdini pants - S", 
+    category: "wind pants", 
+    weight: 88 },
+  { name: "MSR micro-rocket, firestarter, case", 
+    category: "stove", 
+    weight: 122 },
+  { name: "Oiselle distance short, liner cut out - M", 
+    category: "bottoms", 
+    weight: 91 },
+  { name: "Rab Aeon Tee (short sleeve) - women's 12", 
+    category: "top", 
+    weight: 62 },
+]
+
+items.each do |item|
+  Item.create(item)
+end
+
+lists = [
+  { name: "John Muir Trail 2013", 
+    user_id: 1 },
+  { name: "JMT section 2012", 
+    user_id: 2 },
+  { name: "Pacific Crest Trail 2014", 
+    user_id: 1 },
+  { name: "Appalachian Trail 2010", 
+    user_id: 3 },
+  { name: "Winter Backpacking", 
+    user_id: 2 },
+]
+
+lists.each do |list|
+  List.create(list)
+end
