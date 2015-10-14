923511496993514d7af099a8dd04c428187300ca
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-14 10:32:07 -0700

    added test for #create

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 RSpec.describe Api::V1::ListsController, type: :controller do
   describe "POST #create" do
     before :each do
+      @old_count = List.count
       post 'create', :list => { name: "Foo", user_id: 1 }
     end
-    # it "should be successful" do
-    #   # request.env["HTTP_ACCEPT"] = "application/json"
-    # end
+
     it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "adds 1 to the number of Lists" do
+      expect(List.count).to eq(@old_count + 1)
     end
   end
 end
