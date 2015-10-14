179c01a50c2345f4acfbd59c70fe032b9444f2ce
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-14 12:50:04 -0700

    added spec for GET #show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
+  describe "GET #show" do
+    before :each do
+      user1 = create :user
+      get :show, id: user1.id
 
+      @user = JSON.parse response.body
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status :success
+    end
+
+    context "the returned JSON object" do
+      it "has an `user` wrapper" do
+        expect(@user.keys).to eq ["user"]
+      end
+
+      it "includes id, name, category, and weight" do
+        expect(@user["user"].keys).to eq [
+          "id",
+          "username", 
+          "uid"
+          ]
+      end
+    end
+  end
 end
