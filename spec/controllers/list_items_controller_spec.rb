b557817d45ca00d3ae9dbb2620916695db3c6460
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-14 12:56:02 -0700

    added contexts for has list_id and no list_id to GET #index specs

--- spec/controllers/list_items_controller_spec.rb
+++ spec/controllers/list_items_controller_spec.rb
@@ -2,38 +2,57 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ListItemsController, type: :controller do
   describe "GET #index" do
-    let(:list) { create :list }
+    context "no list_id" do
+      before :each do
+        list_item1 = create :list_item, list_id: 1, item_id: 2, quantity: 1
+        list_item2 = create :list_item, list_id: 2, item_id: 4, quantity: 3
 
-    before :each do
-      list_item1 = create :list_item, list_id: list.id, item_id: 2, quantity: 1
-      list_item2 = create :list_item, list_id: list.id, item_id: 4, quantity: 3
-
-      get :index
-      list_items_wrapper = JSON.parse response.body
-      @list_items = list_items_wrapper["list_items"]
-    end
-
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
-
-    context "the returned JSON object" do
-      it "is an array" do
-        expect(@list_items).to be_an_instance_of Array
+        get :index
+        list_items_wrapper = JSON.parse response.body
+        @list_items = list_items_wrapper["list_items"]
       end
 
-      it "is equal in length to the number of Items in the List" do
-        list_items_count = list.list_items.count
-        expect(@list_items.length).to eq(list_items_count)
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
       end
 
-      it "includes name, category, and weight" do
-        expect(@list_items.first.keys).to eq [
-          "id",
-          "list_id", 
-          "item_id", 
-          "quantity"
-          ]
+      context "the returned JSON object" do
+        it "is an array" do
+          expect(@list_items).to be_an_instance_of Array
+        end
+
+        it "includes name, category, and weight" do
+          expect(@list_items.first.keys).to eq [
+            "id",
+            "list_id", 
+            "item_id", 
+            "quantity"
+            ]
+        end
+      end
+    end
+
+    context "has list_id" do
+      let(:list) { create :list }
+
+      before :each do
+        list_item1 = create :list_item, list_id: list.id, item_id: 2, quantity: 1
+        list_item2 = create :list_item, list_id: list.id + 1, item_id: 4, quantity: 3
+
+        get :index, list_id: list.id
+        list_items_wrapper = JSON.parse response.body
+        @list_items = list_items_wrapper["list_items"]
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      context "the returned JSON object" do
+        it "is equal in length to the number of Items in the List" do
+          list_items_count = list.list_items.count
+          expect(@list_items.length).to eq(list_items_count)
+        end
       end
     end
   end
