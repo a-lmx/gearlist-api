a006d4695c3db2b9e36d4cf71ccd66922e0c897c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-14 11:31:46 -0700

    added #index and #show tests; WIP

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -1,6 +1,90 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ListsController, type: :controller do
+  describe "GET #index" do
+    context "no user_id" do
+      before :each do
+        list1 = create :list, name: "JMT", user_id: 1
+        list2 = create :list, name: "PCT", user_id: 1
+
+        get :index
+        lists_wrapper = JSON.parse response.body
+        @lists = lists_wrapper["lists"]
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      context "the returned JSON object" do
+        it "is an array" do
+          expect(@lists).to be_an_instance_of Array
+        end
+
+        it "is equal in length to the number of Items in the db" do
+          expect(@lists.length).to eq(List.count)
+        end
+
+        it "includes name, category, and weight" do
+          expect(@lists.first.keys).to eq [
+            "id",
+            "name", 
+            "user_id"
+            ]
+        end
+      end
+    end
+
+    context "has user_id" do
+      let(:user) { create :user }
+
+      before :each do
+        list1 = create :list, name: "JMT", user_id: user.id
+        list2 = create :list, name: "PCT", user_id: user.id + 1
+
+        get :index
+        lists_wrapper = JSON.parse response.body
+        @lists = lists_wrapper["lists"]
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "only shows the lists for that user" do
+        list_count = List.where(user_id: user.id).length
+        expect(@lists.length).to eq(list_count)
+      end
+    end
+  end
+
+  describe "GET #show" do
+    before :each do
+      list1 = create :list
+      get :show, id: list1.id
+
+      @list = JSON.parse response.body
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status :success
+    end
+
+    context "the returned JSON object" do
+      it "has an `list` wrapper" do
+        expect(@list.keys).to eq ["list"]
+      end
+
+      it "includes id, name, category, and weight" do
+        expect(@list["list"].keys).to eq [
+          "id",
+          "name", 
+          "user_id"
+          ]
+      end
+    end
+  end
+
   describe "POST #create" do
     before :each do
       @old_count = List.count
