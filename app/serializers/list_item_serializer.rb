2511fb68a8b6ecd6d504a242c411f79f9150729d
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-14 11:01:37 -0700

    commented out associations

--- app/serializers/list_item_serializer.rb
+++ app/serializers/list_item_serializer.rb
@@ -1,6 +1,6 @@
 class ListItemSerializer < ActiveModel::Serializer
   attributes :id, :list_id, :item_id, :quantity
 
-  belongs_to :item
-  belongs_to :list
+  # belongs_to :item
+  # belongs_to :list
 end
