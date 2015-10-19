c7f6467a75d3914efa5a861aaec872a4bae58e43
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-19 10:21:27 -0700

    added action for custom_list_items

--- app/controllers/api/v1/list_items_controller.rb
+++ app/controllers/api/v1/list_items_controller.rb
@@ -3,13 +3,20 @@ module Api
     class ListItemsController < ApplicationController
       def index
         if params[:list_id]
-          list = List.find(params[:list_id]) 
+          list = find_list
           list_items = list.list_items
         else
           list_items = ListItem.all
         end
 
         render json: list_items, status: :ok
+      end
+
+      def complete
+        list = find_list
+        list_items = list.list_items
+
+        render json: list_items, each_serializer: CompleteListItemSerializer
       end
 
       def show
@@ -31,6 +38,10 @@ module Api
       end
 
       private
+
+      def find_list
+        List.find(params[:list_id])
+      end
 
       def list_item_params
         params.require(:list_item).permit(:list_id, :item_id, :quantity, :section)
