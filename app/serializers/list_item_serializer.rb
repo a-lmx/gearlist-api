4a9c65e9f117bf2580e10b22d985cc71e435d4a1
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-19 15:42:00 -0700

    updated section to section_id

--- app/serializers/list_item_serializer.rb
+++ app/serializers/list_item_serializer.rb
@@ -1,5 +1,5 @@
 class ListItemSerializer < ActiveModel::Serializer
-  attributes :id, :list_id, :item_id, :quantity, :section
+  attributes :id, :list_id, :item_id, :quantity, :section_id
 
   # belongs_to :item
   # belongs_to :list
