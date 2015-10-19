dfe3b9c92d3eada88f55b8cd2cfdedb8fe999279
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-19 15:41:43 -0700

    updated after new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012210736) do
+ActiveRecord::Schema.define(version: 20151019221650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20151012210736) do
     t.integer  "item_id"
     t.integer  "list_id"
     t.integer  "quantity"
-    t.string   "section"
+    t.integer  "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20151012210736) do
   create_table "lists", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
