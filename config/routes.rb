919b42b7465ddd90c99d0cf435f7cf8d56608762
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-20 19:47:51 -0700

    added routes for list-sections/id/items and items resources

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
     #   get '/lists/:list_id/complete_list_items', to: 'list_items#complete', as: 'complete_list_items'
     #   get '/lists/:list_id/items_by_section', to: 'lists#items_by_section', as: 'items_by_section'
 
-    #   resources :items, only: [:create, :index, :show]
+      get 'list-sections/:list_section_id/items', to: 'list_section_items#index', as: 'complete_list_items'
+
+      resources :items, only: [:create, :index, :show]
 
     #   resources :list_items, only: [:index, :show, :create, :destroy]
     end
