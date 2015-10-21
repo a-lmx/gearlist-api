bb7368c63b18ffe02f41dd15c74057c0b0c22d88
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-20 19:10:18 -0700

    added belongs_to associations

--- app/models/list_section.rb
+++ app/models/list_section.rb
@@ -1,4 +1,7 @@
 class ListSection < ActiveRecord::Base
   # Associations ---------------------------------------------------------------
   has_many :list_section_items
+  
+  belongs_to :list
+  belongs_to :section
 end
