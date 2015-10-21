7a25cf469eb7f91aa549e2526499c9837bde2937
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-21 16:04:53 -0700

    added default format to api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'list_sections/index'
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:show] do
         resources :lists, only: [:index]
