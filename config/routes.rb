b305ee627d2f416eccc36493ff95f013a5a21d78
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-20 19:31:40 -0700

    commented out routes to add back in incrementally

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'list_sections/index'
+
   namespace :api do
     namespace :v1 do
       resources :users, only: [:show] do
@@ -6,16 +8,17 @@ Rails.application.routes.draw do
       end
 
       resources :lists, only: [:index, :show, :create] do
-        resources :list_items, only: [:index]
-        resources :items, only: [:index]
-        resources :sections, only: [:index]
+        get 'sections', to: 'list_sections#index'
+    #     resources :list_items, only: [:index]
+    #     resources :items, only: [:index]
+    #     resources :sections, only: [:index]
       end
-      get '/lists/:list_id/complete_list_items', to: 'list_items#complete', as: 'complete_list_items'
-      get '/lists/:list_id/items_by_section', to: 'lists#items_by_section', as: 'items_by_section'
+    #   get '/lists/:list_id/complete_list_items', to: 'list_items#complete', as: 'complete_list_items'
+    #   get '/lists/:list_id/items_by_section', to: 'lists#items_by_section', as: 'items_by_section'
 
-      resources :items, only: [:create, :index, :show]
+    #   resources :items, only: [:create, :index, :show]
 
-      resources :list_items, only: [:index, :show, :create, :destroy]
+    #   resources :list_items, only: [:index, :show, :create, :destroy]
     end
   end
 end
