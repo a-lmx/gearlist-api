4001f2d334555c66964e57172765bd4204756d7b
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-21 19:44:03 -0700

    removed unused routes, added post route for #add_items

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,13 @@ Rails.application.routes.draw do
 
       resources :lists, only: [:index, :show, :create] do
         get 'sections', to: 'list_sections#index'
-    #     resources :list_items, only: [:index]
-    #     resources :items, only: [:index]
-    #     resources :sections, only: [:index]
+        post 'items', to: 'list_section_items#add_items'
       end
-    #   get '/lists/:list_id/complete_list_items', to: 'list_items#complete', as: 'complete_list_items'
-    #   get '/lists/:list_id/items_by_section', to: 'lists#items_by_section', as: 'items_by_section'
-
+      
       get 'list-sections/:list_section_id/items', to: 'list_section_items#index', as: 'complete_list_items'
 
       resources :items, only: [:create, :index, :show]
 
-    #   resources :list_items, only: [:index, :show, :create, :destroy]
     end
   end
 end
