31fac241e2d5bc327b3f0719510cbbbd8095a568
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-22 23:40:32 -0700

    created #restrict_access method to check auth token in header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::API
-  
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :restrict_access
+
   private
+
+  def restrict_access
+    # token = request.headers["Token"]
+
+    # if token == nil || !User.find_by(token: token)
+    #   render json: { error: 'Bad Token'}, status: 401
+    # end
+
+    authenticate_or_request_with_http_token do |token, options|
+      User.exists?(token: token)
+    end
+  end
 
   def find_list
     @list = List.find_by(id: params[:id] || params[:list_id])
