726ff7df10c88bea5e842ee69c710ae74cc8c0c0
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-22 20:56:59 -0700

    added content to sessions#create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,15 +1,16 @@
 module Api
   module V1
-    class SessionsController < ApplicationController 
-      def new
-        render :new
-      end
-     
+    class SessionsController < ApplicationController      
       def create
-        raise
-        # @auth = request.env['omniauth.auth']['credentials']
-        # redirect_to root_path
-        # render :create
+        id = params["id"]
+        name = params["name"]
+        provider = params["provider"]
+
+        user = User.find_or_create_by_omniauth(id, name, provider)
+        user.set_token
+
+        render  json: { user_id: user.id, token: user.token },
+                status: 200         
       end
     end
   end
