449179fee4d571811d0a34a0abd1cf372565fdf7
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-22 20:57:26 -0700

    created .find_or_create_by_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   has_many :lists
   # Validations ----------------------------------------------------------------
   validates :username, :uid, :provider, presence: true
+
+  def self.find_or_create_by_omniauth(uid, name, provider)
+    user = User.where(uid: uid, name: name, provider: provider).first_or_initialize
+
+    return user.save ? user : nil
+  end
+
+  def set_token
+    
+  end
 end
