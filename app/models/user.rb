b2f9e3f8db659514919e7a0ccfb4599de51dd689
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-22 22:20:14 -0700

    fixed formatting on .find_or_create_by_omniauth; created .generate_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,21 @@ class User < ActiveRecord::Base
   validates :username, :uid, :provider, presence: true
 
   def self.find_or_create_by_omniauth(uid, name, provider)
-    user = User.where(uid: uid, name: name, provider: provider).first_or_initialize
+    user = User.where(
+      uid: uid, 
+      username: name, 
+      provider: provider
+    ).first_or_initialize
+
+    user.token = generate_token
 
     return user.save ? user : nil
   end
 
-  def set_token
-    
+  private
+
+  # mahalo to @lilagrc for this token generator
+  def self.generate_token
+    SecureRandom.base64.tr('+/=', 'Qrt')
   end
 end
