38a1aead524aa4f017bf3d2f603ce6bf10472908
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-24 16:50:58 -0700

    added has_one associations

--- app/models/list_section_item.rb
+++ app/models/list_section_item.rb
@@ -2,6 +2,8 @@ class ListSectionItem < ActiveRecord::Base
   # Associations ---------------------------------------------------------------
   belongs_to :list_section
   belongs_to :item
+  has_one :list, through: :list_section
+  has_one :section, through: :list_section
   # Validations ----------------------------------------------------------------
   validates :list_section_id, :item_id, presence: true
 end
