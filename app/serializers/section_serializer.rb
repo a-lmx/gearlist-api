803ba10fba0a2a5ec4411b57a41a8986f248017d
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-27 13:21:26 -0700

    removed unused custom attributes

--- app/serializers/section_serializer.rb
+++ app/serializers/section_serializer.rb
@@ -1,13 +1,3 @@
 class SectionSerializer < ActiveModel::Serializer
   attributes :id, :name
-
-  has_many :list_section_items, serializer: CompleteListSectionItemSerializer
-
-  def name
-    object.section.name
-  end
-
-  def list_section_items
-    object.list_section_items
-  end
 end
