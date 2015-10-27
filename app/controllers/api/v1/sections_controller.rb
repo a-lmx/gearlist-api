776efe202e6d20826d49c266fc14939569c8a0e5
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-27 13:21:46 -0700

    removed unused check for :list_id in #index

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -2,12 +2,7 @@ module Api
   module V1
     class SectionsController < ApplicationController
       def index
-        if params[:list_id]
-          list = List.find(params[:list_id]) 
-          @sections = list.sections.uniq
-        else
-          @sections = Sections.all
-        end
+        @sections = Section.all
 
         render json: @sections, each_serializer: SectionSerializer, status: :ok
       end
