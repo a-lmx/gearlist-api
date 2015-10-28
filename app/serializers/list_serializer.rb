fde2b21d0ddbb22aa32430310e0257effc60b098
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-10-27 19:40:48 -0700

    added user_name

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,3 +1,7 @@
 class ListSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :secret, :user_id
+  attributes :id, :name, :description, :secret, :user_id, :user_name
+
+  def user_name
+    object.user.username
+  end
 end
